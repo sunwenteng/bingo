dc38859a250878c0c3de034aa5edf62109dea7f9
swt <swt12345>
2018-05-28 21:10:50 +0800

    add test

---
+++ src/test/testDescriptor.ts
@@ -0,0 +1,44 @@
+import {PlayerSession} from "../net/ws/player_session";
+import {World} from "../net/ws/world";
+import {Server} from '../net/ws/web_socket'
+import {Log} from '../util/log'
+import * as util from 'util';
+
+const Config = require('../../config/config.json');
+import {execTime} from '../util/descriptor'
+
+Log.init(__dirname + Config.log.dir, Config.log.level);
+
+class test {
+
+    @execTime()
+    end() {
+        console.log('i am end');
+    }
+
+    @execTime(true)
+    async start(server: Server) {
+        await server.start(PlayerSession);
+        return new Promise<void>((resolve => {
+            setTimeout(async () => {
+                resolve();
+            }, 3000)
+        }));
+    }
+
+    @execTime()
+    async start1() {
+        return new Promise<void>((resolve => {
+            setTimeout(async () => {
+                console.log(111);
+                resolve();
+            }, 5000)
+        }));
+    }
+}
+
+
+let server = new Server(Config.server.host, Config.server.port);
+new test().end();
+new test().start(server);
+new test().start1();
