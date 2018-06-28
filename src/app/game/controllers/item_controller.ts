dc05444b8ee7162018810a95f0681da835dc97fb
swt <swt12345>
2018-06-28 15:31:38 +0800

    ...

---
+++ src/app/game/controllers/item_controller.ts
@@ -0,0 +1,37 @@
+import {C2S, S2C} from "../../proto/cmd";
+import {Role} from "../modles/role";
+import {controller} from "../../../lib/util/descriptor";
+import Time = require('../../../lib/util/time');
+import {GameWorld} from "../game_world";
+import {Log} from "../../../lib/util/log";
+
+export class ItemController {
+    private static _instance: ItemController;
+
+    public static getInstance(): ItemController {
+        if (!this._instance) {
+            this._instance = new ItemController();
+        }
+        return this._instance;
+    }
+
+    @controller()
+    echo(role: Role, msg: C2S.CS_TEST_ECHO) {
+        let pck = S2C.SC_TEST_ECHO.create();
+        pck.msg = '';
+        pck.cmdId = 1;
+        role.sendProtocol(pck);
+    }
+
+    @controller(true)
+    async readAndWrite(role: Role, msg: C2S.CS_TEST_ECHO) {
+
+    }
+
+    @controller()
+    async readonly(role: Role, msg: C2S.CS_TEST_ECHO) {
+
+    }
+
+    createEqu
+}
